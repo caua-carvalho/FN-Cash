981910af49892c7b2cdd44beccbcc1a39afe9643
caua-carvalho <caua.carma033@gmail.com>
2025-03-13 15:21:49 -0300

    header pages

--- pages/header.php
+++ pages/header.php
@@ -1,7 +1,13 @@
+
+<!DOCTYPE html>
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard - Sistema de Gerenciamento Financeiro</title>
+
+    <!-- Icone da pagina -->
+    <link rel="icon" type="image/svg+xml" href="../img/icone.svg">
     
     <!-- Bootstrap 4.6 CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css">
@@ -13,6 +19,13 @@
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet">
     
     <!-- Custom CSS -->
-    <link rel="stylesheet" href="https://github.com/caua-carvalho/FN-Cash/blob/main/pages/css/animation.css">
-    <link rel="stylesheet" href="https://github.com/caua-carvalho/FN-Cash/blob/main/pages/css/custom.css">
+    <link rel="stylesheet" href=".\css\custom.css">
+    <link rel="stylesheet" href=".\css\animation.css">
+
+    <!-- Boostrap icons -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css">
 </head>
+<body>
+    
+</body>
+</html>
