8c2c6995e86babac9d48cf86d1f221d916774cc8
caua-carvalho <caua.carma033@gmail.com>
2025-03-17 14:14:18 -0300

    alteracao no banco de dados

--- banco de dados/banco de dados.sql
+++ banco de dados/banco de dados.sql
@@ -1,6 +1,15 @@
 CREATE DATABASE fncash;
 USE fncash;
 
+CREATE TABLE usuario (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    senha VARCHAR(255) NOT NULL,
+    data_criacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    data_atualizacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
 CREATE TABLE contas (
     id INT AUTO_INCREMENT PRIMARY KEY,
     titulo VARCHAR(100) NOT NULL,
@@ -13,22 +22,7 @@ CREATE TABLE contas (
     tipo VARCHAR(20) NOT NULL COMMENT 'despesa, receita ou transferencia',
     recorrente VARCHAR(3) DEFAULT 'nao',
     comprovante VARCHAR(3) DEFAULT 'nao',
-    data_cadastro TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    data_cadastro TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    icone varchar(100)
 );
 
-CREATE TABLE cartoes (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(100) NOT NULL,
-    bandeira VARCHAR(50) NOT NULL,
-    tipo ENUM('credito', 'debito', 'multiplo') NOT NULL,
-    numero_final VARCHAR(4) NOT NULL,
-    data_validade DATE NOT NULL,
-    limite DECIMAL(10,2) DEFAULT NULL,
-    dia_fechamento INT DEFAULT NULL,
-    dia_vencimento INT DEFAULT NULL,
-    cor VARCHAR(7) DEFAULT '#CCCCCC',
-    ativo TINYINT(1) DEFAULT 1,
-    data_cadastro TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    data_atualizacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    FOREIGN KEY (usuario_id) REFERENCES usuarios(id)
-);
