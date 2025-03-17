ba22c1a2f6edd787777a2164593978e6d994739f
caua-carvalho <caua.carma033@gmail.com>
2025-03-16 21:35:56 -0300

    exibir transacoes

--- funcoes.php
+++ funcoes.php
@@ -1,6 +1,9 @@
 <?php
 require "conexao.php";
 
+$dia_mes = date('d-m');
+$dia_mes_ano = date('d-m-Y');
+
 // CADASTRO DE DESPESAS
 function cadastro_contas($conexao, $titulo, $valor, $data, $categoria, $forma_pagamento, $status, $observacao, $tipo, $recorrente, $comprovante) {
     $stmt = $conexao->prepare("INSERT INTO contas (titulo, valor, data, categoria, forma_pagamento, status, observacao, tipo, recorrente, comprovante) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
@@ -39,7 +42,7 @@ function exibir_saldo(){
     echo "R$ " . $saldo;
 }
 
-// EXIBIR HISTORICO DE TRANSACOES
+// EXIBIR HISTORICO DE TRANSACOES NO DOC TRANSACOES.PHP
 function transacoes($tipo){
     global $conexao;
 
@@ -110,6 +113,55 @@ function transacoes($tipo){
             </td>
         </tr>";
     }
+}
+
+//EXIBIR TRANSACOES NO DOC DASHBOARD.PHP
+function transacoes_simplificada(){
+    global $conexao;
+
+    $sql = "
+            SELECT 
+                id,
+                titulo,
+                CONCAT('R$ ', FORMAT(valor, 2, 'pt_BR')) AS valor_formatado,
+                DATE_FORMAT(data, '%d/%m/%Y') AS data_formatada,
+                categoria,
+                forma_pagamento,
+                status,
+                observacao,
+                tipo,
+                recorrente,
+                comprovante,
+                DATE_FORMAT(data_cadastro, '%d/%m/%Y') AS data_cadastro_formatada,
+                TIME_FORMAT(TIME(data_cadastro), '%H:%i:%s') AS hora_cadastro
+            FROM 
+                contas;";
+    $resultado = $conexao->query($sql);
+    $row = mysqli_fetch_assoc($resultado);
+
+    if($row['data_formatada'] == $dia_mes_ano){
+        $row['data_formatda'] = "hoje"
+    }
+
+    if($resultado->num_rows > 0){
+        foreach($resultado as $row){
+            echo '
+            <li class="transaction-item p-3">
+                <div class="transaction-info">
+                    <div class="transaction-icon expense">
+                        <i class="fas fa-shopping-cart"></i>
+                    </div>
+                    <div class="transaction-details">
+                        <h5>' . $row['titulo'] . '</h5>
+                        <p>' . $row['data_cadastro'] . '</p>
+                    </div>
+                </div>
+                <div class="transaction-amount expense">- R$ 152,35</div>
+            </li>';
+        }
+    }else{
+        echo "0 resultados encontrados";
+    }   
 }
 
 
